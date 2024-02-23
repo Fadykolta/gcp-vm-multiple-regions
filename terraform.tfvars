4fe3a361b9f846d8613ea733ba51b72c7eb37b5d
John Bedeir <jnb@e-boks.dk>
2024-02-23 15:18:36 +0100

    project-tf-files

---
+++ terraform.tfvars
@@ -0,0 +1,9 @@
+regions = ["europe-west3", "europe-west2"] #Frankfurt, London
+
+network_tags = ["pre-prod", "prod"]
+
+#machine_type = "n2-highcpu-48"
+machine_type = "e2-medium"
+#boot_disk_image = "projects/PROJECT_ID/global/images/pexip-conf-v30-1"
+
+boot_disk_image = "ubuntu-os-cloud/ubuntu-2004-lts"
---
+++ variables.tf
@@ -0,0 +1,56 @@
+variable "regions" {
+  description = "The GCP region where to deploy the VM."
+  type        = list(string)
+}
+
+variable "network_tags" {
+  description = "List of network tags to apply to the VM."
+  type        = list(string)
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC to use."
+  type        = string
+  default = "my-vpc"
+}
+
+variable "machine_type" {
+  description = "The machine type of the GCP VM."
+  type        = string
+}
+
+variable "cpu_platform" {
+  description = "Minimum required CPU platform for the VM."
+  default     = "Intel Ice Lake"
+}
+
+variable "boot_disk_image" {
+  description = "The custom image for the boot disk."
+  type        = string
+}
+
+variable "boot_disk_size_gb" {
+  description = "The size of the boot disk in GB."
+  default     = 250
+}
+
+variable "boot_disk_type" {
+  description = "The type of the boot disk."
+  default     = "pd-ssd"
+}
+
+variable "subnets" {
+  description = "A map of regions to subnet configurations"
+  type = map(object({
+    subnet_ip = string
+  }))
+
+  default = {
+    "europe-west3" = {
+      subnet_ip = "10.0.0.0/20"
+    },
+    "europe-west2" = {
+      subnet_ip = "10.0.16.0/20"
+    }
+  }
+}
